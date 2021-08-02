727bcc72b52172bf6bd8422e09c7cdff67d67249
Uday Korat <ukorat@gmail.com>
2021-08-02 14:19:54 -0700

    Update req_ack_4ph_tx.sv
    added synchronizer for ack signal for TX clock domain

--- req_ack_4ph_tx.sv
+++ req_ack_4ph_tx.sv
@@ -13,6 +13,14 @@ module req_ack_4ph_tx
 	output logic [DW-1:0]	dout
 );
 
+  logic [1:0] ack_r;
+	
+//Synchronzing ack with Tx clock
+  always_ff @(posedge clk_tx or negedge rst_b) begin
+    if (!rst_b) ack_r <= 2'b00; //used 2 FF chained sync cell
+    else ack_r <= {ack_r[0], ack};
+  end
+	
   always_ff @(posedge clk_tx or negedge rst_b) begin
     if (!rst_b)         req <= 1'b0;
     else if (val & rdy) req <= 1'b1;
