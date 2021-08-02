446e82d39aed105ff232df3cb19c7ffd918dba51
Uday Korat <ukorat@gmail.com>
2021-08-02 14:16:11 -0700

    Update req_ack_4ph_tx.sv
    identation fixes

--- req_ack_4ph_tx.sv
+++ req_ack_4ph_tx.sv
@@ -16,7 +16,7 @@ module req_ack_4ph_tx
   always_ff @(posedge clk_tx or negedge rst_b) begin
     if (!rst_b)         req <= 1'b0;
     else if (val & rdy) req <= 1'b1;
-		else if (ack) 			req <= 1'b0;
+    else if (ack) 	req <= 1'b0;
     else                req <= req;
     
     assign rdy = ! (req | acq);
